d8870291638d31592416d0f8e949232dd7f915a7
Pufcorina <todorananacorina13@gmail.com>
2018-11-22 13:29:17 +0200

    added blur and gray algos

--- Threads/src/Image.java
+++ Threads/src/Image.java
@@ -77,4 +77,54 @@ public class Image {
             e.printStackTrace();
         }
     }
+
+    private void grayScaleImageFilter() {
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++) {
+                int sum = (int) (r[i][j] * 0.299 + g[i][j] * 0.587 + b[i][j] * 0.114);
+                if (sum > 255)
+                    grayScale[i][j] = 255;
+                else
+                    grayScale[i][j] = sum;
+            }
+    }
+
+    public void exportGrayScaleImageToFile(String fileName) {
+        grayScaleImageFilter();
+        exportImage(fileName, createPixelMatrix(grayScale, grayScale, grayScale));
+    }
+
+    public void exportGaussianBlurImageToFile(String fileName) {
+        gaussianBlurImageFilter();
+        exportImage(fileName, blureMatrix);
+    }
+
+    private void gaussianBlurImageFilter() {
+        for (int i = 0; i < height; i++)
+            for (int j = 0; j < width; j++)
+                blureMatrix[i][j] = getAveragePixel(i, j, 9);
+    }
+
+    private int[] getAveragePixel(int x, int y, int size) {
+        int[] average = new int[3];
+        average[0] = 0;
+        average[1] = 0;
+        average[2] = 0;
+        int pix = 0;
+        for (int i = x - (size / 2); i < x + (size / 2) + 1; i++)
+            if (i >= 0 && i < height)
+                for (int j = y - 1; j < y + 2; j++)
+                    if (j >= 0 && j < width)
+                        if (x != i || y != j) {
+                            pix++;
+                            average[0] += r[i][j];
+                            average[1] += g[i][j];
+                            average[2] += b[i][j];
+                        }
+        average[0] = average[0] / pix;
+        average[1] = average[1] / pix;
+        average[2] = average[2] / pix;
+
+        return average;
+    }
 }
--- Threads/src/Main.java
+++ Threads/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
     public static void main(String[] args) {
         Image i = new Image("images.jpg");
         i.exportImageToFile("final_image");
+        i.exportGaussianBlurImageToFile("blure");
+        i.exportGrayScaleImageToFile("gray");
         System.out.println("Iuliaaaa <3");
     }
 }
